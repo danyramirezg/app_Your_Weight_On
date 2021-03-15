a5232a922cdf11e674104f89e88357ac7f3133b3
danyramirezg <danyramirezg@gmail.com>
2021-03-15 17:07:30 -0500

    App converses your weight on the moon, mars and jupiter

--- app/src/main/java/com/dany/yourweighton/MainActivity.kt
+++ app/src/main/java/com/dany/yourweighton/MainActivity.kt
@@ -1,28 +1,103 @@
 package com.dany.yourweighton
 
-import android.media.tv.TvContract
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.EditText
+import android.text.TextUtils
+import android.util.Log
+import android.view.View
+import android.widget.CheckBox
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+// Adding a new View.OnClick and implement all members
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
-    val weightMoon = 0.165F
+    val moonGravity = 0.165F
+    val marsGravity = 0.38F
+    val jupiterGravity = 2.34f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var input = inputId.text
 
         buttonConvert.setOnClickListener {
-            var result = converseWeight(input.toString().toDouble())
-            resultId.text = "On the moon your weight is $result"
+           // var result = converseWeight(input.toString().toDouble())
+            // resultId.text = "On the moon your weight is $result"
+        }
+
+        // I need to register the checkboxes if I want to see them in Log:
+
+        moonCheckbox.setOnClickListener(this) // this means the class MainActivity
+        marsCheckbox.setOnClickListener(this)
+        jupiterCheckbox.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        v as CheckBox // Casting the CheckBox from activity_main.xml
+        var isChecked: Boolean = v.isChecked
+        var input = inputId.text
+
+        when(v.id){
+            R.id.moonCheckbox -> if (isChecked && !TextUtils.isEmpty(inputId.text.toString())){
+                converseWeight(input.toString().toDouble(), v)
+                marsCheckbox.isChecked = false
+                jupiterCheckbox.isChecked = false
+            }
+            R.id.marsCheckbox -> if (isChecked && !TextUtils.isEmpty(inputId.text.toString())){
+                converseWeight(input.toString().toDouble(), v)
+                moonCheckbox.isChecked = false
+                jupiterCheckbox.isChecked = false
+            }
+            R.id.jupiterCheckbox -> if (isChecked && !TextUtils.isEmpty((inputId.text.toString()))){
+                converseWeight(input.toString().toDouble(), v)
+                moonCheckbox.isChecked = false
+                marsCheckbox.isChecked = false
+            }
+        }
+
+
+
+        // Example: Using Log:
+
+//        when (v.id) {
+//            R.id.moonCheckbox -> if (isChecked) {
+//                Log.d("Setting", "Moon")
+//            } else {
+//                Log.d("Uncheck", "Moon")
+//            }
+//            R.id.marsCheckbox -> if (isChecked) {
+//                Log.d("Setting", "Mars")
+//            } else {
+//                Log.d("Uncheck", "Mars")
+//            }
+//            R.id.jupiterCheckbox -> if (isChecked) {
+//                Log.d("Setting", "Jupiter")
+//            } else {
+//                Log.d("Uncheck", "Jupiter")
+//            }
+//        }
+    }
+
+    fun converseWeight(weight: Double, checkBox: CheckBox) {
+        var result: Double?
+        when(checkBox.id){
+            R.id.moonCheckbox ->  {
+                result = weight * moonGravity
+                                                    // I'm calling the function format (It's below)
+                resultId.text = "On the moon your weight is ${result.format(2)}"
+            }
+            R.id.marsCheckbox -> {
+                result = weight * marsGravity
+                resultId.text = "On mars your weight is ${result.format(2)}"
+            }
+            R.id.jupiterCheckbox -> {
+                result = weight * jupiterGravity
+                resultId.text = "On jupiter your weight is ${result.format(2)}"
+            }
+            else -> result = weight
         }
     }
 
-    fun converseWeight(weight: Double): Double {
-        return weight * weightMoon
-    }
+    // Function to round numbers (to 2 decimals):
+    fun Double.format(digits: Int) = java.lang.String.format("%.${digits}f", this)
 }
